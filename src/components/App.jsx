93231d2d90a4cac2fdf913599189d52118eb24c0
LifeLong-LeArneR17 <snackli17@gmail.com>
2023-03-07 21:10:48 +0200

    fix
    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,19 @@ import { ContactList } from './ContactList/ContactList';
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
     return (
-      JSON.parse(window.localStorage.getItem("savedContacts")) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      JSON.parse(window.localStorage.getItem('savedContacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
       ]
     );
   });
 
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    window.localStorage.setItem("savedContacts", JSON.stringify(contacts));
+    window.localStorage.setItem('savedContacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const handleChange = event => {
@@ -38,28 +38,26 @@ export const App = () => {
   const getFilteredContacts = () => {
     const filteredContactList = contacts.filter(contact => {
       return contact.name.toLowerCase().includes(filter.toLowerCase());
-    })
+    });
     return filteredContactList;
   };
 
-  const handleDelete = (id) => {
-    setContacts(prevContacts => 
-      prevContacts.filter(contact => contact.id !== id),
+  const handleDelete = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
   };
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleChange={handleChange} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
